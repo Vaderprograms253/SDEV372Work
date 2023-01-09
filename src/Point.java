40562051338559ffe2ebee286c9fe6e7c8e45588
Xavier <Denson.Xavier@student.greenriver.edu>
2023-01-08 18:48:35 -0800

    Added Point File

--- src/Point.java
+++ src/Point.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Point {
+public class Point
+{
+    private double x;
+    private double y;
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
 }
