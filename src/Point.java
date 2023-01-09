3837e3891e8797520ab729f345223252b4074d8e
Xavier <Denson.Xavier@student.greenriver.edu>
2023-01-08 18:54:28 -0800

    Generated getters and setters for coordinates X,Y

--- src/Point.java
+++ src/Point.java
@@ -1,5 +1,25 @@
 public class Point
 {
+    public double getX()
+    {
+        return x;
+    }
+
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+
+    public double getY()
+    {
+        return y;
+    }
+
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+
     private double x;
     private double y;
 
